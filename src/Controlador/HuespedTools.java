329755c61243708cf3b1846cd05c0da04220b9f6
andres <andres@AndresakoFijo>
2016-02-09 00:26:14 +0100

    Lunes 8 Noche

--- src/Controlador/HuespedTools.java
+++ src/Controlador/HuespedTools.java
@@ -8,26 +8,29 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.exception.ConstraintViolationException;
 
 /**
  * @author andres
  */
 public class HuespedTools {
 
+    private MyTools MT;
     private ArrayList<Huesped> listaHuespedes;
     private final SessionFactory sessionFactory;
 
     public HuespedTools() {
         sessionFactory = NewHibernateUtil.getSessionFactory();
-        listaHuespedes = new ArrayList<>();
+        MT = new MyTools();
     }
 
     public void RellenarHuespedes() {
-        
+        listaHuespedes = new ArrayList<>();
+
         Session session = sessionFactory.openSession();
         Transaction tx;
         tx = session.beginTransaction();
-        Query q = session.createQuery("from Huesped");
+        Query q = session.createQuery("from Huesped order by dni ASC");
         List<Huesped> lista = q.list();
         Iterator<Huesped> iter = lista.iterator();
         tx.commit();
@@ -39,11 +42,85 @@ public class HuespedTools {
         }
     }
 
+    public void RellenarHuespedes(String msg) {
+        listaHuespedes = new ArrayList<>();
+
+        Session session = sessionFactory.openSession();
+        Transaction tx;
+        tx = session.beginTransaction();
+        Query q = session.createQuery("from Huesped order by dni ASC");
+        List<Huesped> lista = q.list();
+        Iterator<Huesped> iter = lista.iterator();
+
+        while (iter.hasNext()) {
+            Huesped hues = (Huesped) iter.next();
+            if (hues.getDni().contains(msg) || hues.getNombre().contains(msg) || hues.getApellidos().contains(msg)) {
+                listaHuespedes.add(hues);
+            }
+        }
+        tx.commit();
+        session.close();
+    }
+
     public int ContarHuespedes() {
         return listaHuespedes.size();
     }
 
     public Huesped getHuesped(int pos) {
         return listaHuespedes.get(pos - 1);
+    }
+
+    public void addHuesped(Huesped hues) {
+
+        Session session = sessionFactory.openSession();
+        Transaction tx;
+
+        try {
+            tx = session.beginTransaction();
+            session.save(hues);
+            tx.commit();
+
+            MT.mostrarAviso("Añadido correctamente");
+        } catch (ConstraintViolationException e) {
+            MT.mostrarError("Error, el huesped ya existe");
+        } finally {
+            session.close();
+        }
+    }
+
+    public void modHuesped(Huesped hues) {
+
+        Session session = sessionFactory.openSession();
+        Transaction tx;
+
+        try {
+            tx = session.beginTransaction();
+            session.update(hues);
+            tx.commit();
+
+            MT.mostrarAviso("Editado correctamente");
+        } catch (ConstraintViolationException e) {
+            MT.mostrarError("Error al editar");
+        } finally {
+            session.close();
+        }
+
+    }
+
+    public void delActor(Huesped hues) {
+        Session session = sessionFactory.openSession();
+        Transaction tx;
+
+        try {
+            tx = session.beginTransaction();
+            session.delete(hues);
+            tx.commit();
+
+            MT.mostrarAviso("Borrado correctamente");
+        } catch (ConstraintViolationException e) {
+            MT.mostrarError("Error al borrar");
+        } finally {
+            session.close();
+        }
     }
 }
